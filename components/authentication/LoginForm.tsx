18af903aa54a2bef8e00ccba03e8d70ee6821971
Rares Musea <raresmusea001@gmail.com>
2024-10-18 00:13:32 +0300

    Finish layout for login form

--- components/authentication/LoginForm.tsx
+++ components/authentication/LoginForm.tsx
@@ -3,16 +3,22 @@
 import { CardWrapper } from "../CardWrapper";
 import { LoginSchema } from "@/schemas/AuthValidation";
 import { useForm } from "react-hook-form";
+import { useState, useTransition } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import * as z from 'zod';
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { FormError } from "../FormError";
-import { FormSucces, FormSuccess } from "../FormSuccess";
+import { FormSuccess } from "../FormSuccess";
+import { login } from "@/actions/Login";
 
 
 export const LoginForm = () => {
+    const [isPending, startTransition] = useTransition();
+    const [error, setError] = useState<string | undefined>("");
+    const [success, setSuccess] = useState<string | undefined>("");
+
     const form = useForm<z.infer<typeof LoginSchema>>({
         resolver: zodResolver(LoginSchema),
         defaultValues: {
@@ -21,46 +27,61 @@ export const LoginForm = () => {
         }
     });
 
+    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+        setSuccess("");
+        setError("");
+
+        startTransition(() => {
+            login(values)
+                .then(data => {
+                    setError(data.error)
+                    setSuccess(data.success)
+                })
+        });
+    }
+
     return (
         <CardWrapper headerName="Welcome Back! Sign in into your account" backButtonText="Don't you have an account? Register now!" backButtonHref="/register" socialOptionsEnabled>
             <Form {...form}>
                 <form
-                    className="space-y-6" 
-                    onSubmit={form.handleSubmit(() => {})}>
-                        <section className="space-y-4">
-                            <FormField control={form.control} name="email" render={({field}) => (
-                                <FormItem>
-                                    <FormLabel>Email</FormLabel>
-                                    <FormControl>
-                                        <Input 
-                                        {...field} 
+                    className="space-y-6"
+                    onSubmit={form.handleSubmit(onSubmit)}>
+                    <section className="space-y-4">
+                        <FormField control={form.control} name="email" render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        {...field}
+                                        disabled={isPending}
                                         placeholder="Enter your email address"
                                         type="email"
-                                        />
-                                    </FormControl>
-                                    <FormMessage></FormMessage>
-                                </FormItem>
-                            )} />
+                                    />
+                                </FormControl>
+                                <FormMessage></FormMessage>
+                            </FormItem>
+                        )} />
 
-                            <FormField control={form.control} name="password" render={({field}) => (
-                                <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <FormControl>
-                                        <Input 
-                                        {...field} 
+                        <FormField control={form.control} name="password" render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        {...field}
                                         placeholder="Enter your password"
+                                        disabled={isPending}
                                         type="password"
-                                        />
-                                    </FormControl>
-                                    <FormMessage></FormMessage>
-                                </FormItem>
-                            )} />
-                        </section>
-                        <FormSuccess message="" />
-                        <FormError message="" />
-                        <Button type="submit" className="w-full">
-                            Log In
-                        </Button>
+                                    />
+                                </FormControl>
+                                <FormMessage></FormMessage>
+                            </FormItem>
+                        )} />
+                    </section>
+                    <FormSuccess message={success} />
+                    <FormError message={error} />
+                    <Button type="submit" className="w-full" disabled={isPending}>
+                        Log In
+                    </Button>
                 </form>
             </Form>
         </CardWrapper>
