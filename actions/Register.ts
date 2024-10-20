e423f7e1b7029437b11f2ba33454b5634d56bef7
Rares Musea <raresmusea001@gmail.com>
2024-10-20 22:16:37 +0300

    Add functionality for storing registered users within the db

--- actions/Register.ts
+++ actions/Register.ts
@@ -2,17 +2,42 @@
 
 import { RegisterSchema } from '@/schemas/AuthValidation';
 import * as z from 'zod'
+import bcrypt from "bcrypt"
+import { db } from '@/persistency/Db';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
-    
-    if(!validatedFields.success) {
+
+    if (!validatedFields.success) {
         return {
             error: "Invalid fields!"
         };
     }
 
+    const { email, password, name } = validatedFields.data;
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const existingUser = await db.user.findUnique({
+        where: {
+            email,
+        }
+    })
+
+    if (existingUser) {
+        return { error: "The provided email address is already associated to another account!" }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPass
+        }
+    })
+
+    //TODO: Sent verification token via email
+
     return {
-        success: "Email sent!"
+        success: "User created successfully!"
     }
 }
