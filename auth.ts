e864a862f58691420ebbaefdc44dc73e11a8549e
Rares Musea <raresmusea001@gmail.com>
2024-10-28 22:53:34 +0200

    Add middleware edge compatibility with Prisma

--- auth.ts
+++ auth.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
- 
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { db } from "@/persistency/Db"
+
+import authConfig from "./auth.config";
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub, Google],
+  ...authConfig,
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
 });
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,13 @@
-import { auth } from "@/auth"
- 
+import authConfig from "./auth.config";
+import NextAuth from "next-auth"; 
+
+const {auth} = NextAuth(authConfig);
+
 export default auth((req) => {
   console.log(`ROUTE: ${req.nextUrl.pathname}`);
 })
  
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/login"],
+  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',],
 }
