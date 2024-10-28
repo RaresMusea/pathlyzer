0cbb65c89fa5a549b6db3328b249c2bb6b4a14c6
Rares Musea <raresmusea001@gmail.com>
2024-10-28 23:45:29 +0200

    Config middleware routing

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,34 @@
 import authConfig from "./auth.config";
-import NextAuth from "next-auth"; 
+import NextAuth from "next-auth";
 
-const {auth} = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
+import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from "./routes";
 
 export default auth((req) => {
-  console.log(`ROUTE: ${req.nextUrl.pathname}`);
-})
- 
+  const { nextUrl } = req;
+  const isLoggedIn: boolean = !!req.auth;
+  const isApiAuthRoute: boolean = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute: boolean = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute: boolean = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/login", nextUrl));
+  }
+
+  return;
+});
+
 // Optionally, don't invoke Middleware on some paths
 export const config = {
   matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',],
