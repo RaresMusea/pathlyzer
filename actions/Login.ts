7232f6f65bcfd94193aecde27be1f69f787d412f
Rares Musea <raresmusea001@gmail.com>
2024-11-03 23:26:46 +0200

    Handle login and redirect to settings page

--- actions/Login.ts
+++ actions/Login.ts
@@ -2,17 +2,42 @@
 
 import { LoginSchema } from '@/schemas/AuthValidation';
 import * as z from 'zod'
+import { signIn } from '@/auth';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+import { AuthError } from 'next-auth';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
-    
-    if(!validatedFields.success) {
+
+    if (!validatedFields.success) {
         return {
             error: "Invalid fields!"
         };
     }
 
-    return {
-        success: "Email sent!"
+    const { email, password } = validatedFields.data;
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT
+        });
     }
-}
+    catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return {
+                        error: "Invalid login credentials!"
+                    }
+                default:
+                    return {
+                        error: "An error occurred while attempting to sign you in."
+                    }
+            }
+        }
+
+        throw error;
+    }
+};
