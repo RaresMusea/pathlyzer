5537bb135b770f49a449a0fcc10f08b1ca5062e3
Rareș-Gabriel Mușea <77101390+RaresMusea@users.noreply.github.com>
2024-12-10 16:23:42 +0200

    Add roles within application (#8)

--- auth.ts
+++ auth.ts
@@ -1,10 +1,50 @@
-import NextAuth from "next-auth"
+import NextAuth, {DefaultSession} from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "@/persistency/Db"
+import { getUserById } from "@/persistency/data/User";
 
 import authConfig from "./auth.config";
+import { UserRole } from "@prisma/client";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+};
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async jwt({token}) {
+      if (!token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+      return token;
+    },
+
+    async session({token, session}) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    }
+  },
   ...authConfig,
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
