bd11ea26062f061e910fa05b6798abe7aba4c8d1
Rares Musea <raresmusea001@gmail.com>
2024-12-17 00:19:17 +0200

    Add functionality for signing in either with GitHub or Google account

--- auth.config.ts
+++ auth.config.ts
@@ -7,8 +7,14 @@ import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
  
 export default { providers: [
-    Google,
-    GitHub,
+    Google ({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHub({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
     async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
