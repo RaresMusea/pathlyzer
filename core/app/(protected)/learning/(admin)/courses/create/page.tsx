dbdfb783b8e4b4863c27a6cdd6a95acea4bdcaa6
Rares Musea <raresmusea001@gmail.com>
2025-04-12 12:52:16 +0300

    Add TipTap editor extensions

---
+++ core/app/(protected)/learning/(admin)/courses/create/page.tsx
@@ -0,0 +1,11 @@
+import { CourseBuilder } from "@/components/learning/courses/CourseBuilder";
+
+const CourseCreationPage = () => {
+    return (
+        <div className="flex items-center justify-center mt-12">
+            <CourseBuilder/>
+        </div>
+    );
+}
+
+export default CourseCreationPage
---
+++ core/components/learning/courses/CourseBuilder.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const CourseBuilder = () => {
+    return (
+        <div className="flex items-center justify-center mt-12">
+            <div>Course Builder</div>
+        </div>
+    );
+}
