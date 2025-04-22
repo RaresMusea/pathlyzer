67b6c5235de1828db4f9570a8c7b8e0acdc2a3a9
Rares Musea <raresmusea001@gmail.com>
2025-04-23 00:00:49 +0300

    Add a breadcrumbs generator for the dashboard

--- core/app/(protected)/dashboard/layout.tsx
+++ core/app/(protected)/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import { BreadcrumbsGenerator } from "@/components/navigation/BreadcrumbsGenerator";
 import { NavSidebar } from "@/components/navigation/NavSidebar";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
@@ -16,17 +17,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
                             <div className="flex items-center gap-2 px-4">
                                 <SidebarTrigger className="-ml-1" />
                                 <Separator orientation="vertical" className="mr-2 h-4" />
-                                <Breadcrumb>
-                                    <BreadcrumbList>
-                                        <BreadcrumbItem className="hidden md:block">
-                                            <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
-                                        </BreadcrumbItem>
-                                        <BreadcrumbSeparator className="hidden md:block" />
-                                        <BreadcrumbItem>
-                                            <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                                        </BreadcrumbItem>
-                                    </BreadcrumbList>
-                                </Breadcrumb>
+                                <BreadcrumbsGenerator />
                             </div>
                             <div className="pr-3">
                                 <ThemeToggle />
