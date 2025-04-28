5b48ea18c7e2e7979b035b5fb8e181b4f73ded58
Rares Musea <raresmusea001@gmail.com>
2025-04-28 17:32:28 +0300

    Refactoring for user app role context component

--- core/app/(protected)/(appl)/admin/courses/page.tsx
+++ core/app/(protected)/(appl)/admin/courses/page.tsx
@@ -1,16 +1,8 @@
 import { getCourses } from "@/app/service/course/courseService";
-import { auth } from "@/auth";
 import { ManageCoursesPageWrapper } from "@/components/admin/courses/ManageCoursesPageWrapper";
 import { CourseDto } from "@/types/types";
-import { redirect } from "next/navigation";
 
 export default async function ManageCoursesPage() {
-    const session = await auth();
-
-    if (session?.user.role !== "ADMINISTRATOR") {
-        redirect("/unauthorized")
-    };
-
     const courses: CourseDto[] = await getCourses();
 
     return (
