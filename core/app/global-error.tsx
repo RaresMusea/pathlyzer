793a7980d0443abbc1ce88b7aabd8d5c38f9d030
Rares Musea <raresmusea001@gmail.com>
2025-04-29 22:37:00 +0300

    Fix global error refresh state

--- core/app/global-error.tsx
+++ core/app/global-error.tsx
@@ -1,9 +1,10 @@
 'use client';
 
+import { startTransition } from "react";
 import { Navbar } from "@/components/navbar/Navbar";
 import { Button } from "@/components/ui/button";
 import { AlertCircle } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function GlobalError({
@@ -14,11 +15,17 @@ export default function GlobalError({
     reset: () => void
 }) {
     const [isAnimated, setIsAnimated] = useState(false);
-    const router = useRouter();
 
     useEffect(() => {
         setIsAnimated(true)
     }, []);
+
+    const reload = () => {
+        startTransition(() => {
+            window.location.reload();
+            reset();
+        });
+    }
 
     return (
         <section>
@@ -30,10 +37,11 @@ export default function GlobalError({
                 </div>
                 <h2 className="text-2xl font-bold mb-2">{"An unexpected error occurred"}... We are sorry for the inconvenience. Trying our best to fix this issue as soon as possible.</h2>
                 <p className="text-muted-foreground mb-6 max-w-md">
+                    Please try again later.
                 </p>
                 <Button
                     className="button px-4 py-2 bg-[var(--pathlyzer-table-border)] text-white rounded-lg hover:bg-[var(--pathlyzer)] transition-colors hover:text-white"
-                    onClick={router.refresh}
+                    onClick={() => reload()}
                 >
                     Try Again
                 </Button>
