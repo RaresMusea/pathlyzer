e705d2663331b86e45180f24f4c1cdb482dae787
Rares Musea <raresmusea001@gmail.com>
2025-04-30 15:28:59 +0300

    Update Breadcrumbs generator to exclude UUIDs

--- core/app/(protected)/(appl)/courses/page.tsx
+++ core/app/(protected)/(appl)/courses/page.tsx
@@ -4,89 +4,9 @@ import { StaticImageData } from "next/image";
 import { CourseCard } from "@/components/learning/courses/course-card/CourseCard";
 import { UserStats } from "@/components/user/UserStats";
 import { UserProgress } from "@/components/user/UserProgress";
-
-const courses = [
-    {
-        id: 1,
-        title: "JavScript fundamentals",
-        description: "Learn how to create interactive web applications using JavaScript.",
-        difficulty: "Beginner",
-        duration: "4 weeks",
-        progress: 65,
-        image: "/placeholder.svg?height=200&width=350",
-        tags: ["JavaScript", "Web", "Frontend"],
-    },
-    {
-        id: 2,
-        title: "React",
-        description: "Learn the art of building modern reusable web components with React.",
-        difficulty: "Intermediate",
-        duration: "6 weeks",
-        progress: 30,
-        image: reactLogo,
-        tags: ["React", "JavaScript", "UI"],
-    },
-    {
-        id: 3,
-        title: "Node.js & Express",
-        description: "Develop robust fullstack apps using Node.js along with Express.",
-        difficulty: "Intermediate",
-        duration: "5 weeks",
-        progress: 0,
-        image: "/placeholder.svg?height=200&width=350",
-        tags: ["Node.js", "Backend", "API"],
-    },
-    {
-        id: 5,
-        title: "DSA (Data Structures and Algorithms)",
-        description: "Master the fundamentals of data structures and algorithms.",
-        difficulty: "Advanced",
-        duration: "8 weeks",
-        progress: 0,
-        image: "/placeholder.svg?height=200&width=350",
-        tags: ["Algo", "Data structures", "Programming"],
-    },
-    {
-        id: 6,
-        title: "Node.js & Express",
-        description: "Develop robust fullstack apps using Node.js along with Express.",
-        difficulty: "Intermediate",
-        duration: "5 weeks",
-        progress: 0,
-        image: "/placeholder.svg?height=200&width=350",
-        tags: ["Node.js", "Backend", "API"],
-    },
-    {
-        id: 7,
-        title: "DSA (Data Structures and Algorithms)",
-        description: "Master the fundamentals of data structures and algorithms.",
-        difficulty: "Advanced",
-        duration: "8 weeks",
-        progress: 0,
-        image: "/placeholder.svg?height=200&width=350",
-        tags: ["Algo", "Data structures", "Programming"],
-    },
-    {
-        id: 8,
-        title: "Node.js & Express",
-        description: "Develop robust fullstack apps using Node.js along with Express.",
-        difficulty: "Intermediate",
-        duration: "5 weeks",
-        progress: 0,
-        image: "/placeholder.svg?height=200&width=350",
-        tags: ["Node.js", "Backend", "API"],
-    },
-    {
-        id: 9,
-        title: "DSA (Data Structures and Algorithms)",
-        description: "Master the fundamentals of data structures and algorithms.",
-        difficulty: "Advanced",
-        duration: "8 weeks",
-        progress: 0,
-        image: "/placeholder.svg?height=200&width=350",
-        tags: ["Algo", "Data structures", "Programming"],
-    },
-]
+import { CourseDto } from "@/types/types";
+import { getAvailableCourses } from "@/app/service/course/courseService";
+import { PageTransition } from "@/components/misc/animations/PageTransition";
 
 export type CourseProps = {
     id: number;
@@ -99,47 +19,49 @@ export type CourseProps = {
     tags: string[];
 }
 
-export default function CoursesPage() {
+export default async function CoursesPage() {
+    const availableCourses: CourseDto[] = await getAvailableCourses();
     return (
-        <div className="p-6 w-full">
-            <div className="flex flex-col md:flex-row gap-6">
-                <div className="flex-1">
-                    <div className="flex justify-between items-center mb-6">
-                        <h1 className="text-3xl font-bold">Available courses</h1>
-                        <div className="flex items-center gap-2">
+        <PageTransition>
+            <div className="p-6 w-full">
+                <div className="flex flex-col md:flex-row gap-6">
+                    <div className="flex-1">
+                        <div className="flex justify-between items-center mb-6">
+                            <h1 className="text-3xl font-bold">Available courses</h1>
+                            <div className="flex items-center gap-2">
+                            </div>
+                        </div>
+
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                            {availableCourses.map((course) => (
+                                <div key={course.id}>
+                                    <CourseCard course={course} />
+                                </div>
+                            ))}
                         </div>
                     </div>
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                        {courses.map((course) => (
-                            <div key={course.id}>
-                                <CourseCard course={course} />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
-                <div className="md:relative md:w-80 w-full mt-14">
-                    <div className="md:sticky md:top-6 space-y-6">
-                        <UserProgress />
-                        <UserStats />
-                        <Card>
-                            <CardHeader className="pb-2">
-                                <CardTitle className="text-xl">Daily challenge</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="text-sm text-muted-foreground mb-4">
-                                    Solve a DSA quiz and earn bonus XP !
-                                </p>
-                                <button className="w-full bg-primary text-primary-foreground hover:bg-primary/90 py-2 rounded-md font-medium">
-                                    Begin challenge
-                                </button>
-                            </CardContent>
-                        </Card>
+                    <div className="md:relative md:w-80 w-full mt-14">
+                        <div className="md:sticky md:top-6 space-y-6">
+                            <UserProgress />
+                            <UserStats />
+                            <Card>
+                                <CardHeader className="pb-2">
+                                    <CardTitle className="text-xl">Daily challenge</CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                    <p className="text-sm text-muted-foreground mb-4">
+                                        Solve a DSA quiz and earn bonus XP !
+                                    </p>
+                                    <button className="w-full bg-primary text-primary-foreground hover:bg-primary/90 py-2 rounded-md font-medium">
+                                        Begin challenge
+                                    </button>
+                                </CardContent>
+                            </Card>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
+        </PageTransition>
     )
 }
--- core/components/learning/courses/course-card/CourseCard.tsx
+++ core/components/learning/courses/course-card/CourseCard.tsx
@@ -1,38 +1,41 @@
-import { CourseProps } from "@/app/(protected)/(appl)/courses/page";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
-import { ChevronRight, Clock } from "lucide-react";
+import { CourseDto } from "@/types/types";
+import { CourseDifficulty } from "@prisma/client";
+import { ChevronRight, ClipboardPaste, Clock } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 
-export const CourseCard = ({ course }: { course: CourseProps }) => {
+export const CourseCard = ({ course }: { course: CourseDto }) => {
     return (
         <Card key={course.id} className="overflow-hidden hover:shadow-md transition-shadow">
             <div className="relative w-full h-[250px]">
                 <Image
-                    src={course.image}
-                    alt={course.title}
+                    src={course.imageSrc}
+                    alt={course.name}
                     fill
                     className="object-cover w-full h-[250px] rounded-sm"
                 />
-                {course.progress > 0 && (
+                {0 > 0 && (
                     <div className="absolute bottom-0 left-0 right-0 bg-background/90 dark:bg-background/50 backdrop-blur-sm p-2">
                         <div className="flex items-center gap-2">
-                            <Progress value={course.progress} className="h-2" />
-                            <span className="text-xs font-medium">{course.progress}%</span>
+                            <Progress value={0} className="h-2" />
+                            <span className="text-xs font-medium">{0}%</span>
                         </div>
                     </div>
                 )}
             </div>
             <CardHeader className="pb-2">
                 <div className="flex justify-between items-start">
-                    <CardTitle className="text-xl">{course.title}</CardTitle>
+                    <CardTitle className="text-xl">{course.name}</CardTitle>
                     <Badge
                         variant={
-                            course.difficulty === "Beginner"
+                            course.difficulty === CourseDifficulty.BEGINNER
                                 ? "default"
-                                : course.difficulty === "Intermediate"
+                                : course.difficulty === CourseDifficulty.INTERMEDIATE
                                     ? "secondary"
                                     : "destructive"
                         }
@@ -45,8 +48,8 @@ export const CourseCard = ({ course }: { course: CourseProps }) => {
             <CardContent className="pb-2">
                 <div className="flex flex-wrap gap-2">
                     {course.tags.map((tag) => (
-                        <Badge key={tag} variant="outline" className="bg-muted/50">
-                            {tag}
+                        <Badge key={tag.id} variant="outline" className="bg-muted/50">
+                            {tag.name}
                         </Badge>
                     ))}
                 </div>
@@ -54,12 +57,27 @@ export const CourseCard = ({ course }: { course: CourseProps }) => {
             <CardFooter className="flex justify-between items-center">
                 <div className="flex items-center text-sm text-muted-foreground">
                     <Clock className="mr-1 h-4 w-4" />
-                    {course.duration}
+                    35h
                 </div>
-                <button className="text-sm font-medium flex items-center text-primary">
-                    {course.progress > 0 ? "Continue" : "Enroll"}
-                    <ChevronRight className="ml-1 h-4 w-4" />
-                </button>
+
+                <>
+                    {
+                        0 === 0 ?
+                            <Button variant="link">
+                                <Link href={`/courses/learn/${course.id}`}>
+                                <div className="flex flex-row items-center">
+                                    <div>Continue</div>
+                                    <ChevronRight className="ml-1 h-4 w-4" />
+                                </div>
+                                </Link>
+                            </Button>
+                            :
+                            <Button variant="default">
+                                Enroll now
+                                <ClipboardPaste className="ml-1 h-4 w-4" />
+                            </Button>
+                    }
+                </>
             </CardFooter>
         </Card>
     )
