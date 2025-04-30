79c682505b846804cf6f9d75420e34bce2892490
Rares Musea <raresmusea001@gmail.com>
2025-05-01 02:53:54 +0300

    Add cascading delete for Enrollment relations

--- core/components/learning/courses/CoursesWrapper.tsx
+++ core/components/learning/courses/CoursesWrapper.tsx
@@ -58,7 +58,7 @@ export const CoursesWrapper = ({ courses, userEnrollments }: CoursesWrapperProps
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
                 {courses.map((course) => (
                     <div key={course.id}>
                         <CourseCard course={course} setSelectedCourse={setSelectedCourse} enrollment={userEnrollments.find(e => e.courseId === course.id)} />
