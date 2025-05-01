72907bed1114d771251d05f0520364f1b1290e0c
Rares Musea <raresmusea001@gmail.com>
2025-05-01 22:38:53 +0300

    Fix login form required resolver triggerring while authenticating using providers

--- core/components/authentication/LoginForm.tsx
+++ core/components/authentication/LoginForm.tsx
@@ -57,7 +57,7 @@ export const LoginForm = () => {
                 .catch(e => {
                     if (e?.digest?.startsWith("NEXT_REDIRECT")) {
                         return;
-                      }
+                    }
                     setError(`Something went wrong while attempting to log you in.\nMore details: ${e}.`);
                 })
         });
@@ -66,14 +66,15 @@ export const LoginForm = () => {
     return (
         <>
             <Form {...form}>
-                <form className="p-6 md:p-8" onSubmit={form.handleSubmit(onSubmit)}>
-                    <div className="flex flex-col gap-6">
-                        <div className="flex flex-col items-center text-center">
-                            <h1 className="text-2xl font-bold">Welcome back!</h1>
-                            <p className="text-balance text-muted-foreground">
-                                Login to your Pathlyzer account
-                            </p>
-                        </div>
+                <div className="flex flex-col gap-6 mt-5">
+                    <div className="flex flex-col items-center text-center">
+                        <h1 className="text-2xl font-bold">Welcome back!</h1>
+                        <p className="text-balance text-muted-foreground">
+                            Login to your Pathlyzer account
+                        </p>
+                    </div>
+                    <form className="flex flex-col gap-6 p-6 md:p-8" onSubmit={form.handleSubmit(onSubmit)}>
+
                         {!show2FA && (
                             <>
                                 <div className="grid gap-2">
@@ -146,24 +147,28 @@ export const LoginForm = () => {
                         )}
                         <AuthAlert message={success} hasCloseButton={true} type={AlertType.SUCCESS} />
                         <AuthAlert message={error || urlError} hasCloseButton={true} type={AlertType.ERROR} />
+
                         <Button type="submit" className="w-[60%] text-center m-auto font-extrabold">
                             Login
                         </Button>
+                    </form>
+
+                    <div className="flex flex-col gap-4 px-3">
                         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                             <span className="relative z-10 bg-background px-2 text-muted-foreground">
                                 Or continue with
                             </span>
                         </div>
                         <SocialAuthMethods setError={setError} />
-                        <div className="text-center text-sm">
+                        <div className="text-center text-sm mb-5">
                             Don&apos;t have an account?{" "}
                             <Link href="/register" className="underline underline-offset-4">
                                 Sign up
                             </Link>
                         </div>
                     </div>
-                </form>
-            </Form>
+                </div>
+            </Form >
         </>
     )
 }
