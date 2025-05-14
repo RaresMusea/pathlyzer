bb1fcfa4d00d40ebf73e150b5a7a0d04bb080cdf
Rares Musea <raresmusea001@gmail.com>
2025-05-14 21:55:08 +0300

    Design a tabbed view for the third page.Add component-based separation between the exam builder and the exam preview

---
+++ core/components/admin/courses/learning-path-management/lesson/creation/steps/EvaluationForm.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { QuizType } from "@prisma/client";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { EvaluationBuilder } from "../EvaluationBuilder";
+
+const getFormattedType = (quizType: QuizType): string => {
+    return (quizType === QuizType.LESSON_QUIZ) ? 'Lesson quiz' : 'Exam';
+}
+
+export const EvaluationForm = ({ quizType }: { quizType: QuizType }) => {
+    const [activeTab, setActiveTab] = useState<string>(`builder`);
+    return (
+        <motion.div
+            initial={{ x: "100%", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: "-100%", opacity: 0 }}
+            transition={{
+                type: "spring",
+                stiffness: 300,
+                damping: 30,
+            }}
+            className="w-full"
+        >
+            <h4 className="text-lg my-3">Use the following quiz builder in order to provide lesson evaluation content.</h4>
+            <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full mt-4">
+                <TabsList className="grid w-full lg:w-1/3 grid-cols-2">
+                    <TabsTrigger value="builder">{getFormattedType(quizType)} builder</TabsTrigger>
+                    <TabsTrigger value="preview">Preview</TabsTrigger>
+                </TabsList>
+                <TabsContent value="builder" className="mt-6">
+                    <EvaluationBuilder evaluationType={quizType} />
+                </TabsContent>
+            </Tabs>
+        </motion.div>
+    )
+}
