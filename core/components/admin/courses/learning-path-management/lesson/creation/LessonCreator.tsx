d613403f53a8b1bf58137657441d8af23210622b
Rares Musea <raresmusea001@gmail.com>
2025-05-14 21:54:20 +0300

    Add third step for the lesson creator component

--- core/components/admin/courses/learning-path-management/lesson/creation/LessonCreator.tsx
+++ core/components/admin/courses/learning-path-management/lesson/creation/LessonCreator.tsx
@@ -6,6 +6,8 @@ import { useLessonBuilder } from "@/context/LessonBuilderContext";
 import { Form } from "@/components/ui/form";
 import { LessonFormNavigator } from "./LessonFormNavigator";
 import { LessonContentCreator } from "./steps/LessonContentCreator";
+import { EvaluationForm } from "./steps/EvaluationForm";
+import { QuizType } from "@prisma/client";
 
 export const LessonCreator = () => {
     const { currentStep, onSubmit, form } = useLessonBuilder();
@@ -16,7 +18,8 @@ export const LessonCreator = () => {
                     <div className="relative overflow-hidden" style={{ minHeight: "400px" }}>
                         <AnimatePresence mode="sync" initial={false}>
                             {currentStep === 1 && <LessonDetailsForm key="step1" />}
-                            {currentStep === 2 && <LessonContentCreator key="step2"/>}
+                            {currentStep === 2 && <LessonContentCreator key="step2" />}
+                            {currentStep === 3 && <EvaluationForm key="step3" quizType={QuizType.LESSON_QUIZ} />}
                         </AnimatePresence>
                     </div>
                     <LessonFormNavigator />
