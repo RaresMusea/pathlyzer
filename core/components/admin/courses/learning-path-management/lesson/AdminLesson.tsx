0b2b04cf52495100f68605a0b52fa33780d333b7
Rares Musea <raresmusea001@gmail.com>
2025-06-11 21:23:27 +0300

    Replace the popover with a multi-functional dropdown menu within AdminLesson component

--- core/components/admin/courses/learning-path-management/lesson/AdminLesson.tsx
+++ core/components/admin/courses/learning-path-management/lesson/AdminLesson.tsx
@@ -1,71 +1,83 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { DropdownMenu } from "@/components/ui/dropdown-menu";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { LessonDto } from "@/types/types";
-import { DropdownMenuContent, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
-import { PopoverArrow } from "@radix-ui/react-popover";
-import { BookOpen } from "lucide-react";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { BookOpen, Trash2 } from "lucide-react";
+import { LessonDto } from "@/types/types";
+import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
+import { LessonDeletionModal } from "./deletion/LessonDeletionModal";
 
-export const AdminLesson = ({ lesson, index, totalAmount }: { lesson: LessonDto, index: number, totalAmount: number }) => {
-    const [popoverOpen, setPopoverOpen] = useState(false);
+export const AdminLesson = ({
+    lesson,
+    index,
+    totalAmount,
+}: {
+    lesson: LessonDto;
+    index: number;
+    totalAmount: number;
+}) => {
+    const [lessonDeletionModalOpen, setLessonDeletionModalOpen] = useState(false);
 
     const cycleLength = 8;
     const cycleIndex = index % cycleLength;
-    let indentationLevel;
 
-    if (cycleIndex <= 2) {
-        indentationLevel = cycleIndex;
-    }
-    else if (cycleIndex <= 4) {
-        indentationLevel = 4 - cycleIndex;
-    } else if (cycleIndex <= 6) {
-        indentationLevel = 4 - cycleIndex;
-    }
-    else {
-        indentationLevel = cycleIndex - 8;
-    }
+    const indentationLevel =
+        cycleIndex <= 2
+            ? cycleIndex
+            : cycleIndex <= 4
+                ? 4 - cycleIndex
+                : cycleIndex <= 6
+                    ? 4 - cycleIndex
+                    : cycleIndex - 8;
 
     const rightPosition = indentationLevel * 40;
     const isFirst = index === 0;
-    const isLast = index === totalAmount;
-    console.log(isLast);
 
     return (
-        <div className="relative"
+        <div
+            className="relative"
             style={{
                 right: `${rightPosition}px`,
                 marginTop: `${isFirst ? 60 : 24}px`,
-            }}>
+            }}
+        >
             <div className="h-[102px] w-[102px] relative">
 
-                <Popover onOpenChange={setPopoverOpen} open={popoverOpen}>
-                    <DropdownMenu>
-                        <PopoverTrigger onMouseEnter={() => { if (popoverOpen) { setPopoverOpen(false); } setPopoverOpen(true) }} onMouseLeave={() => setPopoverOpen(false)} asChild>
-                            <DropdownMenuTrigger>
-                            <Button size="rounded" variant="lockedLesson"
-                                className="h-[70px] w-[70px] border-b-8">
-                                <BookOpen className={"h-10 w-10  text-neutral-400 stroke-neutral-400"} />
-                            </Button>
-                            </DropdownMenuTrigger>
-                        </PopoverTrigger>
-                        <PopoverContent
-                            onMouseEnter={() => setPopoverOpen(true)}
-                            onMouseLeave={() => setTimeout(() => setPopoverOpen(false), 150)}
-                            className="w-70 font-nunito"
+                <DropdownMenu modal={false}>
+                    <DropdownMenuTrigger asChild>
+                        <div
                         >
-                            <h4 className="text-lg font-bold">Lesson {index + 1}:  {lesson.title}</h4>
-                            <p className="text-sm">{lesson.description}</p>
-                            <PopoverArrow className="fill-gray-300 dark:fill-gray-900" />
-                        </PopoverContent>
-                        <DropdownMenuContent>
+                            <Button
+                                size="rounded"
+                                variant="lockedLesson"
+                                className="h-[70px] w-[70px] border-b-8"
+                            >
+                                <BookOpen className="h-10 w-10 text-neutral-400 stroke-neutral-400" />
+                            </Button>
+                        </div>
+                    </DropdownMenuTrigger>
 
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </Popover>
-            </div >
+                    <DropdownMenuContent side="bottom" align="start" sideOffset={8} className="font-nunito">
+                        <div className="p-2">Lesson {index + 1}: {lesson.title}
+                        </div>
+                        <DropdownMenuSeparator className="border-b"/>
+                        <DropdownMenuItem onSelect={() => {}}>
+                            
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="text-red-500" onSelect={() => setLessonDeletionModalOpen(true)}>
+                            <Trash2 className="mr-2 h-4 w-4 " />
+                            Delete lesson
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+            <LessonDeletionModal open={lessonDeletionModalOpen} lessonId={lesson.id} setOpen={setLessonDeletionModalOpen} lessonTitle={lesson.title} />
         </div>
-    )
-}
+    );
+};
