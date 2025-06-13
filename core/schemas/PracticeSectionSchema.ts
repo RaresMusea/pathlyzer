81f30b14ac132ec11a2f246b3215215cc78841ff
Rares-Gabriel Musea <rares-gabriel.musea@continental-corporation.com>
2025-06-13 16:06:32 +0300

    Add zod validation schema for the Lesson practice items

---
+++ core/schemas/PracticeSectionSchema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+const PracticeSectionSchema = z.object({
+  title: z.string().min(5, "The title is too short!").max(100, "The title is too long!"),
+  content: z.string().min(10, "The content of the card should contain at least 10 characters!").max(1000, "The content of the card should not exceed 1000 characters!"),
+  duration: z.number().min(10, "The minimum duration is 10 seconds!").max(30, "The maximum duration is 30 seconds!"),
+});
+
+type PracticeSectionForm = z.infer<typeof PracticeSectionSchema>;
