c1346532fb64538714b03582a5da3b9703ba0452
Rares Musea <raresmusea001@gmail.com>
2025-06-18 17:03:31 +0300

    Provide nav UI for the lesson practice page

---
+++ core/app/(protected)/courses/learn/[courseId]/lesson/[lessonId]/practice/not-found.tsx
@@ -0,0 +1,8 @@
+import { LessonPracticeNotFound } from "@/components/misc/not-found/LessonPracticeNotFound";
+import { QuizNotFound } from "@/components/misc/not-found/QuizNotFound";
+
+export default function LessonPracticeNotFoundPage() {
+    return (
+        <LessonPracticeNotFound backUrl=".." backText="Back to lesson" />
+    )
+}
--- core/app/(protected)/courses/learn/[courseId]/lesson/[lessonId]/practice/page.tsx
+++ core/app/(protected)/courses/learn/[courseId]/lesson/[lessonId]/practice/page.tsx
@@ -1,3 +1,19 @@
+import { getLessonPracticeByLessonId } from "@/app/service/learning/lessons/practice/lessonPracticeService";
+import { LessonPracticeWrapper } from "@/components/learning/courses/lesson/practice/LessonPracticeWrapper";
+import { LessonPracticeDto } from "@/types/types";
+import { notFound } from "next/navigation";
+
 export default async function LessonPracticePage({ params }: { params: Promise<{ courseId: string, lessonId: string }> }) {
-    
+    const { courseId, lessonId } = await params;
+    const practiceCard: LessonPracticeDto | null = await getLessonPracticeByLessonId(lessonId);
+
+    if (!courseId || !lessonId || !practiceCard) {
+        notFound();
+    }
+
+    const totalDuration = practiceCard.items.reduce((sum, section) => sum + section.duration, 0);
+
+    return (
+        <LessonPracticeWrapper practiceItems={practiceCard.items} totalDuration={totalDuration} />
+    )
 }
